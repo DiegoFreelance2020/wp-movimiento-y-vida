f4d6f8f1405d3be529e66a3cb2b64899ab7b99db
Angel Burgos Desktop <angelpublicista.1@gmail.com>
2021-04-28 12:42:08 -0500

    Fin page categories

--- master-template-woo-child/assets/js/custom-child.js
+++ master-template-woo-child/assets/js/custom-child.js
@@ -5,4 +5,39 @@ jQuery(function ($) {
     <path d="M30.1147 46.1145L33.8853 49.8852L51.7707 31.9998L33.8853 14.1145L30.1147 17.8852L41.5627 29.3332H16V34.6665H41.5627L30.1147 46.1145Z" fill="white"/>
     </svg>
     </div>`)
+    
+    $('.slick-cats').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 3,
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    })
 });
--- master-template-woo-child/functions.php
+++ master-template-woo-child/functions.php
@@ -7,8 +7,11 @@ function enqueue_styles_child_theme() {
 
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( $child_style, get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version'));
+	wp_enqueue_style( 'slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css', '1.0' );
+	wp_enqueue_style( 'slick-theme-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css', '1.0' );
 	wp_enqueue_style( 'custom-style-css', get_stylesheet_directory_uri() . '/assets/css/custom-style.css', '1.0' );
-	wp_enqueue_script( 'custom-child-js', get_stylesheet_directory_uri() . '/assets/js/custom-child.js', array('jquery'), '1.0', true);
+	wp_enqueue_script( 'custom-child-js', get_stylesheet_directory_uri() . '/assets/js/custom-child.js', array('jquery', 'slick-js'), '1.0', true);
+	wp_enqueue_script( 'slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'), '1.0', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_styles_child_theme' );
@@ -25,3 +28,4 @@ add_filter( 'get_the_archive_title', function ( $title ) {
 
 require "inc/helpers.php";
 require "inc/shortcodes/sc-post-wp.php";
+require "inc/shortcodes/sc-cats-slider.php";
